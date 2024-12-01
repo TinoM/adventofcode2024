bbf3044e3734375974204652219e22142c56c3c1
TinoM <TinoM@users.noreply.github.com>
2024-12-01 16:37:07 +0100

    remove multiplication

--- src/day1.rs
+++ src/day1.rs
@@ -1,39 +1,38 @@
 use itertools::Itertools;
 
-type Pair = (usize,usize);
+type Lists = (Vec<u32>, Vec<u32>);
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<Pair> {
-    input
-        .lines()
-        .map(|l| {
-            let mut pair = l
-                .trim()
-                .split_ascii_whitespace()
-                .map(|d| d.parse().unwrap());
-            (pair.next().unwrap(), pair.next().unwrap())
-        })
-        .collect()
+pub fn input_generator(input: &str) -> Lists {
+    let mut a = Vec::with_capacity(1000);
+    let mut b = Vec::with_capacity(1000);
+    input.lines().for_each(|l| {
+        let mut pair = l
+        // .trim()
+        .split_ascii_whitespace()
+        .map(|d| d.parse().unwrap());
+        a.push(pair.next().unwrap());
+        b.push(pair.next().unwrap());
+    });
+    (a, b)
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &[Pair]) -> usize {
-    input
+pub fn part1(input: &Lists) -> u32 {
+    // input.0.sort();
+    input.0
         .iter()
-        .sorted_by_key(|p| p.0)
-        .zip(input.iter().sorted_by_key(|p| p.1))
-        .map(|x| x.0 .0.abs_diff(x.1 .1))
+        .sorted_unstable()
+        .zip(input.1.iter().sorted_unstable())
+        .map(|(&a,&b)| a.abs_diff(b))
         .sum()
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &[Pair]) -> usize {
-    let mut counts = [0;100000];
-    input.iter().for_each(|i|counts[i.1]+=1);
-    input
-        .iter()
-        .map(|p| p.0 * counts[p.1])
-        .sum()
+pub fn part2(input: &Lists) -> u32 { 
+    let mut counts = [0; 100000];
+    input.1.iter().for_each(|&i| counts[i as usize] += i);
+    input.0.iter().map(|&p| counts[p as usize]).sum()
 }
 
 #[cfg(test)]
@@ -44,7 +43,7 @@ mod tests {
     // A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
     fn example1() {
         assert_eq!(
-            solve_part1(&input_generator(
+            part1(&input_generator(
                 "3   4
 4   3
 2   5
@@ -59,11 +58,16 @@ mod tests {
     #[test]
     // A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
     fn example2() {
-        assert_eq!(solve_part2(&input_generator(                "3   4
+        assert_eq!(
+            part2(&input_generator(
+                "3   4
         4   3
         2   5
         1   3
         3   9
-        3   3")), 31);
+        3   3"
+            )),
+            31
+        );
     }
 }
