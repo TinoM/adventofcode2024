4ee10e06d3b3f48e2e895d452d98244ffcbb6b56
TinoM <TinoM@users.noreply.github.com>
2024-12-03 10:40:47 +0100

    maybe a bit faster

--- src/day2.rs
+++ src/day2.rs
@@ -43,8 +43,35 @@ fn check_report(report: &[u8]) -> bool {
     true
 }
 
+fn check_report_opt(report: &[u8]) -> (bool,usize) {
+    let mut c = None;
+    let mut idx1 = 0;
+    let mut idx2 = 1;
+    while idx2 < report.len() {
+        match report[idx1].cmp(&report[idx2]) {
+            std::cmp::Ordering::Equal => {
+                return (false,idx1);
+            }
+            diff => {
+                if report[idx1].abs_diff(report[idx2]) > 3 {
+                    return (false,idx1);
+                } else if let Some(diff_prev) = c {
+                    if diff != diff_prev {
+                        return (false,idx1);
+                    }
+                } else {
+                    c = Some(diff);
+                }
+            }
+        }
+        idx1 += 1;
+        idx2 += 1;
+    }
+    (true,0)
+}
+
 #[aoc(day2, part2)]
-fn part2(input: &str) -> usize {
+fn part2_old(input: &str) -> usize {
     let mut safe_reports = 0;
     for line in input.lines() {
         let report = line
@@ -60,6 +87,41 @@ fn part2(input: &str) -> usize {
                 if check_report(&report) {
                     safe_reports += 1;
                     break;
+               }
+            }
+        }
+    }
+    safe_reports
+}
+
+#[aoc(day2, part2,optimize)]
+fn part2(input: &str) -> usize {
+    let mut safe_reports = 0;
+    for line in input.lines() {
+        let report = line
+            .split_ascii_whitespace()
+            .flat_map(|x| x.parse())
+            .collect::<Vec<u8>>();
+        let check =check_report_opt(&report);
+        if  check.0 {
+            // eprintln!("safe report: {:?}",report);
+            safe_reports += 1;
+        } else {
+            let mut report2 = report.clone();
+            report2.remove(check.1);
+            // eprintln!("unsafe report: {:?},trying {:?}",report,report2);
+            if check_report(&report2) {
+                safe_reports += 1;
+                // eprintln!("safed report: {:?}",report2);
+                continue;
+            }
+            if check.1 < report.len()-1 {
+                let mut report2 = report.clone();
+                report2.remove(check.1+1);
+                // eprintln!("unsafe report: {:?},trying {:?}",report,report2);
+                if check_report(&report2) {
+                    safe_reports += 1;
+                    // eprintln!("safed report: {:?}",report2);
                 }
             }
         }
@@ -81,6 +143,22 @@ mod tests {
 
     #[test]
     fn part2_example() {
+        assert_eq!(
+            part2("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"),
+            4
+        );
+    }
+
+    #[test]
+    fn test_part2_old() {
+        assert_eq!(
+            part2_old("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"),
+            4
+        );
+    }
+
+    #[test]
+    fn test_part2_speed() {
         assert_eq!(
             part2("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"),
             4
