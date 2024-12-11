dfa1e84a409960ef2890dd8da9c4a5ca3f73bcb3
TinoM <TinoM@users.noreply.github.com>
2024-12-11 16:11:43 +0100

    faster part 1

--- src/day10.rs
+++ src/day10.rs
@@ -5,40 +5,42 @@ use aoc_runner_derive::aoc;
 #[aoc(day10, part1)]
 pub fn part1(input: &str) -> usize {
     let grid = input.lines().map(|l| l.chars().flat_map(|c|c.to_digit(10)).collect::<Vec<_>>()).collect::<Vec<_>>();
-    let mut trailheads:HashMap<(usize,usize),HashSet<(usize,usize)>> = HashMap::new();
-    let mut frontier = vec![];
+    let mut starts = vec![];
     for (y,line) in grid.iter().enumerate() {
         for (x,c) in line.iter().enumerate() {
             if c == &0 {
                 // trailheads.insert((x,y), 0);
-                frontier.push((x,y,(x,y))); 
+                starts.push((x,y)); 
             }
         }
     }
-    while let Some(current) = frontier.pop() {
-        // eprintln!("Current {:?}",current);
-        if grid[current.1][current.0] == 9 {
-            trailheads.entry(current.2).and_modify(|e| {e.insert((current.0,current.1));}).or_insert({
-                let mut set = HashSet::new();
-                set.insert((current.0,current.1));
-                set
-            });
-            continue;
+    // eprintln!("Trailheads {:?}",starts.len());
+    let mut solution = 0;
+    for start in starts {
+        let mut frontier = vec![(start.0,start.1)];
+        let mut solutions = HashSet::new();
+        while let Some(current) = frontier.pop() {
+            // eprintln!("Current {:?}",current);
+            if grid[current.1][current.0] == 9 {
+                solutions.insert(current);
+                continue;
+            }
+            if current.0 > 0 && grid[current.1][current.0-1] == grid[current.1][current.0]+1 {
+                frontier.push((current.0-1,current.1));
+            }
+            if current.0 < grid[0].len()-1 && grid[current.1][current.0+1] == grid[current.1][current.0]+1 {
+                frontier.push((current.0+1,current.1));
+            }
+            if current.1 > 0 && grid[current.1-1][current.0] == grid[current.1][current.0]+1 {
+                frontier.push((current.0,current.1-1));
+            }
+            if current.1 < grid.len()-1 && grid[current.1+1][current.0] == grid[current.1][current.0]+1 {
+                frontier.push((current.0,current.1+1));
+            }
         }
-        if current.0 > 0 && grid[current.1][current.0-1] == grid[current.1][current.0]+1 {
-            frontier.push((current.0-1,current.1,current.2));
-        }
-        if current.0 < grid[0].len()-1 && grid[current.1][current.0+1] == grid[current.1][current.0]+1 {
-            frontier.push((current.0+1,current.1,current.2));
-        }
-        if current.1 > 0 && grid[current.1-1][current.0] == grid[current.1][current.0]+1 {
-            frontier.push((current.0,current.1-1,current.2));
-        }
-        if current.1 < grid.len()-1 && grid[current.1+1][current.0] == grid[current.1][current.0]+1 {
-            frontier.push((current.0,current.1+1,current.2));
-        }
+        solution += solutions.len();
     }
-    trailheads.iter().map(|th|th.1.len()).sum()
+    solution
 }
 
 #[aoc(day10, part2)]
