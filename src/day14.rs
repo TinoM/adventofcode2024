6b2f53debbe64aeb479b3ec775f0f65a5c396dc0
TinoM <TinoM@users.noreply.github.com>
2024-12-14 08:07:21 +0100

    faster

--- src/day14.rs
+++ src/day14.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 
 use aoc_runner_derive::aoc;
 use regex::Regex;
@@ -8,54 +7,35 @@ const HEIGHT:i32 = 103;
 
 #[aoc(day14, part1)]
 pub fn part1(input: &str) -> i32 {
-
-    let mut robots = Regex::new(r"p=(\d+),(\d+) v=(-?\d+),(-?\d+)")
+    let (mut q1,mut q2,mut q3,mut q4) = (0,0,0,0);
+    Regex::new(r"p=(\d+),(\d+) v=(-?\d+),(-?\d+)")
         .unwrap()
         .captures_iter(input)
-        .map(|cap| {
+        .for_each(|cap| {
             let x = cap[1].parse::<i32>().unwrap();
             let y = cap[2].parse::<i32>().unwrap();
-            let vx = cap[3].parse::<i32>().unwrap();
-            let vy = cap[4].parse::<i32>().unwrap();
-            (x, y, vx, vy)
-        })
-        .collect::<Vec<(i32, i32, i32, i32)>>();
-    for r in robots.iter_mut() {
-        if r.2 < 0 {
-            r.0 = (r.0 + (100 * (WIDTH + r.2))) % WIDTH;
-        } else {
-            r.0 = (r.0 + (100 * r.2)) % WIDTH;
-        }
-        if r.3 < 0 {
-            r.1 = (r.1 + (100 * (HEIGHT + r.3))) % HEIGHT;
-        } else {
-            r.1 = (r.1 + (100 * r.3)) % HEIGHT;
-        }
-    }
-    let mut set = HashMap::new();
-    for r in robots.iter() {
-        set.entry((r.0, r.1)).and_modify(|c| *c += 1).or_insert(1);
-    }
-    let q1: i32 = set
-        .iter()
-        .filter(|r| r.0 .0 < WIDTH / 2 && r.0 .1 < HEIGHT / 2)
-        .map(|x| x.1)
-        .sum();
-    let q2: i32 = set
-        .iter()
-        .filter(|r| r.0 .0 > WIDTH / 2 && r.0 .1 < HEIGHT / 2)
-        .map(|x| x.1)
-        .sum();
-    let q3: i32 = set
-        .iter()
-        .filter(|r| r.0 .0 < WIDTH / 2 && r.0 .1 > HEIGHT / 2)
-        .map(|x| x.1)
-        .sum();
-    let q4: i32 = set
-        .iter()
-        .filter(|r| r.0 .0 > WIDTH / 2 && r.0 .1 > HEIGHT / 2)
-        .map(|x| x.1)
-        .sum();
+            let mut vx = cap[3].parse::<i32>().unwrap();
+            let mut vy = cap[4].parse::<i32>().unwrap();
+            if vx < 0 {
+                vx = WIDTH as i32+vx;
+            }
+            if vy < 0 {
+                vy = HEIGHT as i32+vy;
+            }
+            let x = (x + (100 * vx)) % WIDTH;
+            let y = (y + (100 * vy)) % HEIGHT;
+            // eprintln!("{} {} {} {}", x, y,vx,vy);
+            if x < WIDTH/2 && y < HEIGHT/2 {
+                q1 += 1;
+            } else if x > WIDTH/2 && y < HEIGHT/2 {
+                q2 += 1;
+            } else if x < WIDTH/2 && y > HEIGHT/2 {
+                q3 += 1;
+            } else if x > WIDTH/2 && y > HEIGHT/2 {
+                q4 += 1;
+            }
+        });
+    
     // eprintln!("Q1: {} Q2: {} Q3: {} Q4: {}", q1, q2, q3, q4);
     q1 * q2 * q3 * q4
 }
@@ -70,23 +50,21 @@ pub fn part2(input: &str) -> i32 {
         .map(|cap| {
             let x = cap[1].parse::<i32>().unwrap();
             let y = cap[2].parse::<i32>().unwrap();
-            let vx = cap[3].parse::<i32>().unwrap();
-            let vy = cap[4].parse::<i32>().unwrap();
+            let mut vx = cap[3].parse::<i32>().unwrap();
+            let mut vy = cap[4].parse::<i32>().unwrap();
+            if vx < 0 {
+                vx = WIDTH as i32+vx;
+            }
+            if vy < 0 {
+                vy = HEIGHT as i32+vy;
+            }
             (x, y, vx, vy)
         })
         .collect::<Vec<(i32, i32, i32, i32)>>();
     'second: for second in 1..=WIDTH * HEIGHT {
         for r in robots.iter_mut() {
-            if r.2 < 0 {
-                r.0 = (r.0 + (1 * (WIDTH + r.2))) % WIDTH;
-            } else {
-                r.0 = (r.0 + (1 * r.2)) % WIDTH;
-            }
-            if r.3 < 0 {
-                r.1 = (r.1 + (1 * (HEIGHT + r.3))) % HEIGHT;
-            } else {
-                r.1 = (r.1 + (1 * r.3)) % HEIGHT;
-            }
+            r.0 = (r.0 + (1 * r.2)) % WIDTH;
+            r.1 = (r.1 + (1 * r.3)) % HEIGHT;
         }
         let mut set = [false;WIDTH as usize*HEIGHT as usize];
         for r in robots.iter() {
